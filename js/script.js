e378ca4e330bb29a7fe5319b679acacfa75198f0
Nattawat Teaiwtan <Nao.Nait@gmail.com>
2022-03-31 13:04:45 +0700

    edit login link navbar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-$("#carouselButton").click(function(){
-    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-        $("#mycarousel").carousel('pause');
-        $("#carouselButton").children("span").removeClass('fa-pause');
-        $("#carouselButton").children("span").addClass('fa-play');
-    }
-    else if ($("#carouselButton").children("span").hasClass('fa-play')){
-        $("#mycarousel").carousel('cycle');
-        $("#carouselButton").children("span").removeClass('fa-play');
-        $("#carouselButton").children("span").addClass('fa-pause');                    
-    }
-});
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+// $("#carouselButton").click(function(){
+//     if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+//         $("#mycarousel").carousel('pause');
+//         $("#carouselButton").children("span").removeClass('fa-pause');
+//         $("#carouselButton").children("span").addClass('fa-play');
+//     }
+//     else if ($("#carouselButton").children("span").hasClass('fa-play')){
+//         $("#mycarousel").carousel('cycle');
+//         $("#carouselButton").children("span").removeClass('fa-play');
+//         $("#carouselButton").children("span").addClass('fa-pause');                    
+//     }
+// });
